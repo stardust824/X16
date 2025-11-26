626997e559d8c2b8de92b08a3046cd2aed0ec0d0
Ash Summers <wolfmoonzoe@gmail.com>
2025-11-25 21:38:56 -0800

    Adds bits.c functions. Untested

--- bits.c
+++ bits.c
@@ -2,37 +2,66 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include "bits.h"
+#include <math.h>
 
 
 
 // Get the nth bit
 uint16_t getbit(uint16_t number, int n) {
-    // TODO: Your code here
-    return 0;
+    // grab value by bit shifting and & it
+    uint16_t bit = (1 & (number >> n));
+    return bit;
 }
 
 // Get bits that are the given number of bits wide
 uint16_t getbits(uint16_t number, int n, int wide) {
-    // TODO: Your code here
-    return 0;
+    uint16_t mask = 0;
+    // for loop to make bit mask
+    for (int i = 0; i < wide; i++) {
+       // mask += 10^i 
+       mask += round(pow((float) 10.0, (float) i));
+    }
+    // have correct mask now
+    // grab bits and shift them
+    return ((number & mask) >> n);
 }
 
 // Set the nth bit to the given bit value and return the result
 uint16_t setbit(uint16_t number, int n) {
-    // TODO: Your code here
-    return 0;
+    uint16_t mask = 1;
+    // nth bit of mask is 1
+    mask = (mask << n);
+    // ORing it with the mask should set it to 1
+    return (mask | number);
 }
 
 // Clear the nth bit
 uint16_t clearbit(uint16_t number, int n) {
-    // TODO: Your code here
-    return 0;
+    uint16_t mask = 1;
+    // if the bit is 1
+    if(getbit(number, n) == 1) {
+       mask = (mask << n);
+       return (mask ^ number);
+    }
+    // if the bit is already 0, it can just be returned
+    return number;
 }
 
 // Sign extend a number of the given bits to 16 bits
 uint16_t sign_extend(uint16_t x, int bit_count) {
-    // TODO: Your code here
-    return 0;
+    // if 1, can do a for loop like earlier
+    if (getbit(x, (bit_count -1) == 1)) {
+        // set them
+        for (int i = bit_count; i < 16; i++) {
+            setbit(x, i);
+        }
+    } else {
+        // clear them
+        for (int i = 0; i < 16; i++) {
+            clearbit(x, i);
+        }
+    }
+    return x;
 }
 
 bool is_positive(uint16_t number) {
